f6e830252d5c785a2cd4a18251799903854cb19f
HCarrr <huda012354@gmail.com>
2026-04-28 08:13:57 +0700

    final

---
+++ BankTransferConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class BankTransferConfig
+{
+	public Transfer transfer {  get; set; }
+	public Confirmation confirmation { get; set; }
+    public string lang { get; set; }
+	public string method { get; set; }
+	
+
+	class Transfer()
+	{
+        public double threshold { get; set; }
+        public double low_fee { get; set; }
+        public int high_fee { get; set; }
+        
+	}
+
+	class Confirmation()
+	{
+		public string en {  get; set; }
+		public string id { get; set; }
+	}
+
+	public BankTransferConfig()
+	{}
+	public static BankTransferConfig LoadConfig()
+	{
+        BankTransferConfig defaultConfig = new BankTransferConfig
+		{
+			thresshold 
+		}
+
+    }
+
+}
+
+class 
